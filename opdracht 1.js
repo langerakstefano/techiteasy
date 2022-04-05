f2de017b5da8455d73fa765fef0948924f2c6e7a
langerakstefano <langerakstefano@outlook.com>
2022-04-05 16:39:13 +0200

    opdracht 1AenB klaar

---
+++ opdracht 1.js
@@ -0,0 +1,23 @@
+// #### Opdracht 1 - Array Methoden
+//
+// * **Opdracht 1a:** Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de
+// console.
+// * **Opdracht 1b:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht
+// zijn. Log de uitkomst in de console.
+// * **Opdracht 1c:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight
+// beschikken. Log de uitkomst in de console.
+// * **Opdracht 1d:** Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de
+// console.
+
+
+// Opdracht 1A
+// const allTvNames = inventory.filter ( (inventor) => {
+//     console.log(inventor.name)
+// })
+
+// Opdracht 1B
+// const TvsSoldout = inventory.filter ( (inventor) => {
+//     return inventor.originalStock === inventor.sold;
+// })
+// console.log(TvsSoldout)
+
